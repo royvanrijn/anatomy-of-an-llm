91a5e1c9bacdcc0e753cfe588c0e5b5a36ad75c2
Roy van Rijn <roy.van.rijn@gmail.com>
2026-05-22 17:15:32 +0200

    Preparing to put public and launch GH pages for review (added BETA tag)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 
 export default defineConfig({
+  site: "https://royvanrijn.github.io",
+  base: "/TheAnatomyofanLLM",
   integrations: [svelte()]
 });
